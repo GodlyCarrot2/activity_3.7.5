632bf1ea0c134bb043f541101254598e63989b6d
GodlyCarrot2 <rquan80@ladueschools.net>
2023-01-10 18:37:10 +0000

    HI MR J

--- LibraryRunner.java
+++ LibraryRunner.java
@@ -1,4 +1,3 @@
-
 /*
  * Activity 3.7.5
  */
@@ -8,6 +7,26 @@ public class LibraryRunner
 {
   public static void main(String[] arg)
   {
-    
+
+    //Steps 6 and 7
+    Library library = new Library();
+    ArrayList<Book> childrensBooks = library.getChildrensBooks();
+    String authorToFind = "L. Frank Baum";
+    for (Book book : childrensBooks) {
+      if (authorToFind.equals(book.getAuthor())) {
+        System.out.println(book.getTitle());
+      }
+    }
+
+    //Step 8, 9 and 10
+    String bookToFind = "Sky Island";
+    Double minimumRating = 0.0;
+    for (Book book : childrensBooks) {
+      if (bookToFind.equals(book.getTitle())) {
+        minimumRating = book.getRating();
+        System.out.println(minimumRating);
+      }
+    }
+
   }
 }
